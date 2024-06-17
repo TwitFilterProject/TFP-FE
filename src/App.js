9136dc44170de07903e88d730cf847231ab0e7c8
seoyoon04 <sykim8483@naver.com>
2024-06-17 17:36:26 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Main from './page/Main';
+
+function App() {
+  return (
+    <div className="App">
+      <Main />
+    </div>
+  );
+}
+
+export default App;
